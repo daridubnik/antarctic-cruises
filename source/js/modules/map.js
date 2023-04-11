909ec03984fcdd6202e4aecb2baa86348126bcb1
Daria Dubnikova <daridubnik@gmail.com>
2023-04-12 00:48:15 +0300

    Устраняет ошибки lint

--- source/js/modules/map.js
+++ source/js/modules/map.js
@@ -1,19 +1,21 @@
-let center = [59.93866382862344, 30.322931431755293];
+const initMap = () => {
+  const ymaps = window.ymaps;
 
-function initMap() {
-  let map = new ymaps.Map('map', {
-    center: center,
-    zoom: 17
+  ymaps.ready(function () {
+    let map = new ymaps.Map('map', {
+      center: [59.93866382862344, 30.322931431755293],
+      zoom: 16,
+    });
+
+    let placemark = new ymaps.Placemark([59.93866382862344, 30.322931431755293], {}, {
+      iconLayout: 'default#image',
+      iconImageHref: './img/png/marker.png',
+      iconImageSize: [22, 27],
+      iconImageOffset: [-30, -15],
+    });
+
+    map.geoObjects.add(placemark);
   });
-
-  let placemark = new ymaps.Placemark(center, {}, {
-		iconLayout: 'default#image',
-		iconImageHref: './../../../img/png/marker.png',
-		iconImageSize: [22, 27],
-		iconImageOffset: [-30, -15]
-	});
-
-  map.geoObjects.add(placemark);
-}
+};
 
 export {initMap};
