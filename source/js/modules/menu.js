b2ebac5cde0d84b8162d247b5e24c331177e020e
Daria Dubnikova <daridubnik@gmail.com>
2023-04-16 19:59:35 +0300

    Вносит правки после код-ревью и готовит проект к тестированию

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -2,14 +2,39 @@ let body = document.body;
 let header = document.querySelector('.header__wrapper');
 let menu = document.querySelector('.menu');
 let toggle = document.querySelector('.menu__toggle');
-
+let links = document.querySelectorAll('[data-menu-link]');
 
 const initMobileMenu = () => {
-  toggle.addEventListener('click', function () {
-    body.classList.toggle('screen-lock');
-    header.classList.toggle('menu-opened');
-    menu.classList.toggle('is-active');
-  });
+
+  if (document.documentElement.clientWidth < 768) {
+    const toggleMenu = () => {
+      body.classList.toggle('screen-lock');
+      header.classList.toggle('menu-opened');
+      menu.classList.toggle('is-active');
+    };
+
+    toggle.addEventListener('click', function (e) {
+      e.stopPropagation();
+      toggleMenu();
+    });
+
+    document.addEventListener('click', function (e) {
+      const target = e.target;
+      const itsMenu = target === menu || menu.contains(target);
+      const itsToggle = target === toggle;
+      const menuIsActive = menu.classList.contains('is-active');
+
+      if (!itsMenu && !itsToggle && menuIsActive) {
+        toggleMenu();
+      }
+    });
+
+    links.forEach((link) => {
+      link.addEventListener('click', function () {
+        toggleMenu();
+      });
+    });
+  }
 };
 
 export {initMobileMenu};
